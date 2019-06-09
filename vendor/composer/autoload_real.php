c75e70c107ede6bf0410f58f900ebb493b89622b
Jon Atxa <atxajon@gmail.com>
2019-06-09 18:22:29 +0200

    added required drupal/console

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -24,7 +24,7 @@ class ComposerAutoloaderInitc69050299f61ebe269508b29e7cb824b
         spl_autoload_unregister(array('ComposerAutoloaderInitc69050299f61ebe269508b29e7cb824b', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
+        $includePaths[] = get_include_path();
         set_include_path(implode(PATH_SEPARATOR, $includePaths));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
